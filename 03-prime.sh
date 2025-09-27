f889b6b14899803e177c252b635751896e248294
Rajkumar <rajkumarmanda@outlook.com>
2025-09-27 20:59:31 +0530

    roboshop

--- 03-prime.sh
+++ 03-prime.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-# To print Prime Numbers
-# Author is Raj
-# Date-27th
-# Version = V1
+# Print Prime Numbers from 1 to 15
+# Author: Raj
+# Date: 27th
+# Version: V1
 
-for i in {1..15}
+for i in {2..15}  # start from 2 (1 is not prime)
 do
+    prime=1   # assume number is prime
+    for ((j=2; j<i; j++))
+    do
+        if (( i % j == 0 )); then
+            prime=0   # not prime
+            break
+        fi
+    done
 
-    if [ `expr $i % 1` == $i ]; then 
-	echo "$i"
-
+    if (( prime == 1 )); then
+        echo "$i"
     fi
-
 done
