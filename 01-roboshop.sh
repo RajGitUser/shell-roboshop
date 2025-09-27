2ec37ea76258c04a8e824fb050d85b5c13b2de19
Rajkumar <rajkumarmanda@outlook.com>
2025-09-27 19:35:53 +0530

    roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -5,7 +5,7 @@ SD_ID="sg-08e9a23d01ec385fb"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SD_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=AWStest}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SD_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     
     if [ "$instance" != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceId,PrivateIpAddress]' --output text)
@@ -14,6 +14,6 @@ do
 
     fi
 
-    echo $instance: $IP
+    echo "$instance: $IP"
 
 done
