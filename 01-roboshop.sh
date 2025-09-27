e22f0f461dfe1b922766bbebd4a47ce7e2a422cd
Rajkumar <rajkumarmanda@outlook.com>
2025-09-27 21:40:22 +0530

    roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -10,11 +10,11 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SD_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     
     if [ "$instance" != "frontend" ]; then
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceId,PrivateIpAddress]' --output text)
-    RECORD_NAME="$instance.$DOMINE_NAME"   #it will be like mongodb.rajkumardaws.space
-    else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceId,PublicIpAddress]' --output text)
-    RECORD_NAME="$DOMINE_NAME"
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceId,PrivateIpAddress]' --output text)
+        RECORD_NAME="$instance.$DOMINE_NAME"   #it will be like mongodb.rajkumardaws.space
+     else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].[InstanceId,PublicIpAddress]' --output text)
+        RECORD_NAME="$DOMINE_NAME"
     fi
 
     echo "$instance: $IP"
