b3abde2da2f9e97440fff5d02b96b3e3d9519a7d
Rajkumar <rajkumarmanda@outlook.com>
2025-09-28 07:07:19 +0530

    roboshop

--- mysql.sh
+++ mysql.sh
@@ -30,16 +30,16 @@ VALIDATE(){  # functions recieves inputs through orgs just like shell orgs
     fi
 }
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Installing mysql Server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOG_FILE
 VALIDATE $? "Enabling musqld"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Starting mysqld"
 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
 VALIDATE $? "Setting up root Passwd"
 
 END_TIME=$(date +%s)
