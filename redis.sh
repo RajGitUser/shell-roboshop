fcbe8bbcc06b6e7f2c9d1371e6884981db9e5c8b
Rajkumar <rajkumarmanda@outlook.com>
2025-09-28 06:55:57 +0530

    roboshop

---
+++ redis.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+MONGODB_HOST="mongodb.rajkumardaws.space"
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/mongo-logs.log
+START_TIME=$(Date +%s)
+
+
+mkdir -p $LOGS_FOLDER
+echo "script started excecuted at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR :: Please Run this command in SUDO privilage"
+    exit1 # Failure is other than 0
+fi
+
+VALIDATE(){  # functions recieves inputs through orgs just like shell orgs
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+     else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling default Redis"
+
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling redis version7"
+
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing redis" 
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Changing the redis configuration"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling redis"
+
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Restarting redis"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
